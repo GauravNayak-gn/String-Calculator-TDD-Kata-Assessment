580688e30542a8d0adb8471d21e56f7a3c160762
Gaurav Nayak <gaurav.nayakcr@gmail.com>
2025-09-18 22:34:06 +0530

    final commit

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,10 +2,10 @@ class StringCalculator {
   add(numbers) {
     if (!numbers) return 0;
     return numbers
-  .split(/,|\n/)
-  .map(n => parseInt(n, 10))
-  .filter(n => !isNaN(n) && n <= 1000)
-  .reduce((a, b) => a + b, 0);
+      .split(/,|\n/)
+      .map((n) => parseInt(n, 10))
+      .filter((n) => !isNaN(n) && n <= 1000)
+      .reduce((a, b) => a + b, 0);
   }
 }
 
